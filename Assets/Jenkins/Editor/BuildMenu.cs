660af5f9bbc3592f7398ca322f9f0830aecedb5f
WenRong <hybrid0930@gmail.com>
2021-07-13 01:54:21 +0800

    [add] Export Asset

---
+++ Assets/Jenkins/Editor/BuildMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace hyhy.Jenkins
+{
+    public class BuildMenu : MonoBehaviour
+    {
+        private static string BuildFolder
+        {
+            get { return Directory.GetParent(Application.dataPath).FullName.Replace('\\', '/') + "/Builds"; }
+        }
+
+        [MenuItem("Builds/Export Asset")]
+        public static void BuildExportAsset()
+        {
+            Jenkins.ExportAsset(BuildFolder);
+        }
+
+        [MenuItem("Builds/Android")]
+        public static void BuildAndroid()
+        {
+            Jenkins.BuildAndroid(BuildFolder);
+        }
+
+        [MenuItem("Builds/iOS")]
+        public static void BuildiOS()
+        {
+            Jenkins.BuildiOS(BuildFolder);
+        }
+    }
+}
--- Assets/Jenkins/Editor/Jenkins.cs
+++ Assets/Jenkins/Editor/Jenkins.cs
@@ -5,9 +5,9 @@ using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
-namespace JenkinsBuild
+namespace hyhy.Jenkins
 {
-    public class JenkinsBuild
+    public class Jenkins
     {
         private static readonly string GameName = PlayerSettings.productName;
         private static readonly string GameVersion = Application.version.Replace(".", "");
@@ -26,6 +26,9 @@ namespace JenkinsBuild
                 buildLocation = arguments[arguments.IndexOf("-buildPath") + 1];
             else
                 throw new ArgumentException("Project must be launched with a -buildPath argument! Where would we build the project too otherwise?");
+
+            if (arguments.Contains("-exportasset"))
+                ExportAsset(buildLocation);
 
             if (arguments.Contains("-windows32"))
                 BuildWindows32(buildLocation);
@@ -47,6 +50,11 @@ namespace JenkinsBuild
 
             if (arguments.Contains("-webgl"))
                 BuildWebGL(buildLocation);
+        }
+        public static void ExportAsset(string location)
+        {
+            string path = Path.Combine(location, "release.unitypackage").Replace('\\', '/');
+            AssetDatabase.ExportPackage("Assets/Unity-Calendar", path, ExportPackageOptions.Recurse | ExportPackageOptions.IncludeDependencies);
         }
 
         public static void BuildWindows32(string location)
@@ -115,7 +123,7 @@ namespace JenkinsBuild
                 locationPathName = path,
                 target = BuildTarget.Android
             };
-            var keystore = Resources.Load<AndroidKeyStore>("AndroidKeyStore");
+            var keystore = Resources.Load<KeyStore>("AndroidKeyStore");
             if (keystore != null)
             {
                 PlayerSettings.Android.keystoreName = keystore.KeystoreName;
